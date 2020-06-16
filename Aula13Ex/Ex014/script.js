8498d9963c915a905f13c8cbad68d15d548cbd5b
Victor Augusto <vdbernardes@gmail.com>
2020-06-16 11:09:29 -0300

    asd

---
+++ Aula13Ex/Ex014/script.js
@@ -0,0 +1,20 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} horas`
+    if(hora >= 0 && hora < 12) {
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#e5cd50'
+    } else{
+        if(hora >= 12 && hora < 19){
+            img.src = 'fototarde.png'
+            document.body.style.background = '#fd9526'
+        } else{
+            img.src = 'fotonoite.png'
+            document.body.style.background = '#113267'
+        }
+    }
+}
